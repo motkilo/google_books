eedbb13b1062ebeec29602a9986223433646da60
motkilo <code22@motkilo.com>
2019-05-14 17:54:02 -0400

    general updates to code

--- models/book.js
+++ models/book.js
@@ -3,9 +3,13 @@ const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
   title: { type: String, required: true },
-  author: { type: String, required: true },
+  authors: { type: [String], required: true },
   synopsis: String,
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now },
+  image: {type: String, required: true},
+  link: {type: String, required: true},
+  googleId: {type: String, required: true, unique: true}
+
 });
 
 const Book = mongoose.model("Book", bookSchema);

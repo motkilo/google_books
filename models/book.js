2d7c80d5b6e0e27c14fef2f2649dfa5fb960c64a
motkilo <code22@motkilo.com>
2019-05-14 17:56:46 -0400

    updated the book model

--- models/book.js
+++ models/book.js
@@ -4,11 +4,13 @@ const Schema = mongoose.Schema;
 const bookSchema = new Schema({
   title: { type: String, required: true },
   authors: { type: [String], required: true },
-  synopsis: String,
+  description: String,
   date: { type: Date, default: Date.now },
   image: {type: String, required: true},
   link: {type: String, required: true},
   googleId: {type: String, required: true, unique: true}
+
+  // Is a unique _id needed?
 
 });
 
